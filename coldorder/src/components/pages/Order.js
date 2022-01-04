8d24a2347e381331fa2e400cbb0c5a57964778ad
Harviator <78506767+harviator@users.noreply.github.com>
2022-01-04 12:57:09 -0500

    Added some pseudo code for the oder page to get it started

--- coldorder/src/components/pages/Order.js
+++ coldorder/src/components/pages/Order.js
@@ -1,9 +1,35 @@
 import React from 'react';
 
+/*
+const products = [
+    {
+        name: "Coffee",
+        upc: "123456789",
+        productCode: "string",
+        category: "string",
+        certification: "string",
+        size: "string",
+        caseSize: 1,
+        unitCost: 1,
+        discountedUnitCost: 1,
+        productImageURL: "string",
+    }
+];
+*/
+
 function Order(props) {
     return (
         <div className="order">
             <h2>Order</h2>
+            {/* 
+            {products.map(products) => (
+                Some UI here
+                image src={products.productImageURL} alt="{products.name}"
+                Product details (name etc.)
+
+                UI for the 'Add' button
+            )}
+            */}
         </div>
     );
 }
@@ -12,5 +38,5 @@ export default Order;
 
 /*
 Notes:
-- Iterate through the be order items and display them
+- Iterate (map?) through the be items and display them
 */
