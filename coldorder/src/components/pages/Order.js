248600f3b15d6be1697fd0f040aabc27823102e0
Harviator <78506767+harviator@users.noreply.github.com>
2022-01-04 12:37:46 -0500

    Added the file structure for the components and started an order page

---
+++ coldorder/src/components/pages/Order.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Order(props) {
+    return (
+        <div className="order">
+            <h2>Order</h2>
+        </div>
+    );
+}
+
+export default Order;
+
+/*
+Notes:
+- Iterate through the be order items and display them
+*/
