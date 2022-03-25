97a6e07898cfc14263fe2e7cdb1dc53c93e5ecca
Kevin Crawford <crawfordkevin101@gmail.com>
2022-03-24 20:50:56 -0400

    created basic template for the order page

--- client/src/pages/Order.js
+++ client/src/pages/Order.js
@@ -1,4 +1,18 @@
-import React from 'react';
+import * as React from 'react';
+
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const theme = createTheme();
 
 /*
 const products = [
@@ -20,7 +34,6 @@ const products = [
 function Order(props) {
     return (
         <div className="order">
-            <h2>Order</h2>
             {/* 
             {products.map(products) => (
                 Some UI here
@@ -30,6 +43,42 @@ function Order(props) {
                 UI for the 'Add' button
             )}
             */}
+            <ThemeProvider theme={theme}>
+                <Container sx={{ py: 8 }} maxWidth="md">
+                    <Grid container spacing={4}> 
+                        {cards.map((card) => (
+                            <Grid item key={card} xs={12} sm={6} md={4}>
+                                <Card
+                                    sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                                >
+                                    <CardMedia
+                                        component="img"
+                                        sx={{
+                                            // 16:9
+                                            pt: '56.25%',
+                                        }}
+                                        image="https://source.unsplash.com/random"
+                                        alt="random"
+                                    />
+                                    <CardContent sx={{ flexGrow: 1 }}>
+                                        <Typography gutterBottom variant="h5" component="h2">
+                                            Product
+                                        </Typography>
+                                        <Typography>
+                                            This is the product description
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions style={{margin:'auto'}}>
+                                        <Button size="small">Add to cart</Button>
+                                    </CardActions>
+                                </Card>
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Container>
+            </ThemeProvider>
+
+
         </div>
     );
 }
